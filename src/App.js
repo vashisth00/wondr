0956c1b71dc024d518b5ab582bf565416351116b
vashisht.bhushan <vashisht.bhushan@brightlifecare.com>
2020-05-09 04:28:38 +0530

    Final Pull grif

--- src/App.js
+++ src/App.js
@@ -1,40 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './app.scss'
 
-const BASE_URL = "https://picsum.photos/v2/list";
+var CHF ="https://picsum.photos/id/870/200/300?grayscale&blur=2"
 
-class App extends React.Component {
-  state={
-    posts:[]
-  }
+const BASE_URL = "https://picsum.photos/v2/list?page=2&limit=100";
 
-  componentDidMount(){
-    fetch(BASE_URL)
-    .then(res => res.json())
-    .then(posts =>{
-      this.setState({posts});
-    })
-  }
-  render(){
+const UnsplashImage = ({ url, key }) => (
+  <div className="image-item" key={key} >
+    <img src={url} />
+  </div>
+);
+
+let Collage = () => {
+  const [images, setImages] = React.useState([]);
+  const [loaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = (count = 10) => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey =
+      "a22f61e98da4efa25d8860e77a91a596867dd335ecdf7feb12e086943db9565a";
+
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(res => {
+        setImages([...images, ...res.data]);
+        setIsLoaded(true);
+
+        console.log(images);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Wondr Image Fetch</h1>
-       <img src="./logo.svg"></img>
-      </header>
-      <p>
-          Images Grid
-          </p>
-          {this.state.posts.map(post=>(
-            <>
-            <img src={post.download_url} width={post.width/8} height={post.height/8}/>
-            </>
-          ))}
-    </div>
+    <div className="hero is-fullheight is-bold is-info">
+      <div className="hero-body">
+        <div className="container">
+          <div className="header content">
+            <h2 className="subtitle is-6">Code Challenge #16</h2>
+            <h1 className="title is-1">
+              Infinite Scroll Unsplash Code Challenge
+            </h1>
+          </div>
 
+          <InfiniteScroll
+            dataLength={images}
+            next={() => fetchImages(5)}
+            hasMore={true}
+            loader={
+              <img
+                src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+                alt="loading"
+              />
+            }
+          >
+            <div className="image-grid" style={{ marginTop: "30px" }}>
+              {loaded
+                ? images.map((image, index) => (
+                    <UnsplashImage
+                      url={image.urls.regular}
+                      key={index}
+                    />
+                  ))
+                : ""}
+            </div>
+          </InfiniteScroll>
+        </div>
+      </div>
+    </div>
   );
-}
-}
+};
+
+ReactDOM.render(<Collage />, document.getElementById("root"));
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,85 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
+import './App.css';
+import logo from './0.png'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    
-  </React.StrictMode>,
-  document.getElementById('root')
+import ReactDOM from 'react-dom';
+import './app.scss'
+
+var CHF ="https://picsum.photos/id/870/200/300?grayscale&blur=2"
+
+const BASE_URL = "https://picsum.photos/v2/list?page=2&limit=100";
+
+const UnsplashImage = ({ url }) => (
+  <div className="image-item">
+    <img src={url} />
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let Collage = () => {
+  const [images, setImages] = React.useState([]);
+  const [loaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetchImages();
+  }, []);
+
+
+
+  const fetchImages = (count = 100) => {
+    const apiRoot = "https://picsum.photos/v2/";
+    const accessKey =
+      "a22f61e98da4efa25d8860e77a91a596867dd335ecdf7feb12e086943db9565a";
+
+    axios
+      .get(`${apiRoot}list?page=2&limit=${count}`)
+      .then(res => {
+        setImages([...images, ...res.data]);
+        setIsLoaded(true);
+
+        console.log(images);
+      });
+  };
+
+  return (
+    <div className="hero is-fullheight is-bold is-info">
+      <div className="hero-body">
+        <div className="container">
+          <div className="header content">
+            <h2 className="subtitle is-6">Code Challenge #16</h2>
+            <h1 className="title is-1">
+              Infinite Scroll Unsplash Code Challenge
+            </h1>
+            <img src={logo}/>
+          </div>
+
+          <InfiniteScroll
+            dataLength={images}
+            next={() => fetchImages(5)}
+            hasMore={true}
+            loader={
+              <img
+                src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+                alt="loading"
+              />
+            }
+          >
+            <div className="image-grid" style={{ marginTop: "30px" }}>
+              {loaded
+                ? images.map((image) => (
+                    <UnsplashImage
+                      url={image.download_url}
+                    />
+                  ))
+                : ""}
+            </div>
+          </InfiniteScroll>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Collage />, document.getElementById("root"));
+
