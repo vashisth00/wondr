53eb88ecf1cc400f22aa41739a4af8cbd3e3af4c
vashisht.bhushan <vashisht.bhushan@brightlifecare.com>
2020-05-08 21:23:42 +0530

    height width fetch up img

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 
 const BASE_URL = "https://picsum.photos/v2/list";
 
-function App() {
+class App extends React.Component {
+  state={
+    posts:[]
+  }
+
+  componentDidMount(){
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(posts =>{
+      this.setState({posts});
+    })
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -14,8 +26,15 @@ function App() {
       <p>
           Images Grid
           </p>
+          {this.state.posts.map(post=>(
+            <>
+            <img src={post.download_url} width={post.width/8} height={post.height/8}/>
+            </>
+          ))}
     </div>
+
   );
+}
 }
 
 export default App;
